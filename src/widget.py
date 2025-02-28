424c916626c1495012e51c3a89405290142d2e42
alexsandrov16 <al3x5.dev@gmail.com>
2025-02-28 14:50:54 -0500

    Add sidebar function

--- src/widget.py
+++ src/widget.py
@@ -17,7 +17,7 @@ class Modal:
             self.dialog = ft.AlertDialog(
                 modal=modal,
                 title=ft.Text(title),
-                content=ft.Text(content),
+                content=content,
                 actions=action,
                 actions_alignment=ft.MainAxisAlignment.END,
                 #on_dismiss=lambda e: page.add(
@@ -25,3 +25,34 @@ class Modal:
                 #),
                 inset_padding=ft.padding.symmetric(horizontal=40)
             )
+
+# Navegación
+def sidebar(navigation,update):
+    return ft.NavigationRail(
+        selected_index=0,
+        label_type=ft.NavigationRailLabelType.ALL,
+        group_alignment=-0.8,
+        leading=ft.FloatingActionButton(icon=ft.Icons.UPDATE, text="Actualizar",on_click=update),
+        destinations=[
+            ft.NavigationRailDestination(
+                icon=ft.Icons.DASHBOARD_OUTLINED,
+                selected_icon=ft.Icons.DASHBOARD
+            ),ft.NavigationRailDestination(
+                icon=ft.Icons.ACCOUNT_BALANCE_OUTLINED,
+                selected_icon=ft.Icons.ACCOUNT_BALANCE
+            ),ft.NavigationRailDestination(
+                icon=ft.Icons.PAYMENTS_OUTLINED,
+                selected_icon=ft.Icons.PAYMENTS
+            ),ft.NavigationRailDestination(
+                icon=ft.Icons.ACCESS_TIME,
+                selected_icon=ft.Icons.ACCESS_TIME_FILLED_OUTLINED
+            ),ft.NavigationRailDestination(
+                icon=ft.Icons.WARNING_AMBER_ROUNDED,
+                selected_icon=ft.Icons.WARNING_ROUNDED
+            ),ft.NavigationRailDestination(
+                icon=ft.Icons.CREDIT_CARD_OFF_OUTLINED,
+                selected_icon=ft.Icons.CREDIT_CARD_OFF
+            ),
+        ],
+        on_change=lambda e: navigation(e)
+    )
