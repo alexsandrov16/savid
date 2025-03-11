8a54a82f8942a6f95d7c03b3ecea85627efe4245
alexsandrov16 <al3x5.dev@gmail.com>
2025-03-11 08:56:29 -0400

    Fixed config

--- src/config.py
+++ src/config.py
@@ -1,13 +1,31 @@
 import json
+import os
+import sys
+import db
 
-with open('config.json',encoding='utf-8') as f:
+# Determina la ruta base
+if getattr(sys, 'frozen', False):
+    # Si está ejecutándose como un ejecutable
+    base_path = os.path.dirname(sys.executable)
+else:
+    # Si está ejecutándose desde el código fuente
+    base_path = os.path.dirname(__file__)
+
+config_path = os.path.join(base_path, 'config.json')
+
+with open(config_path,encoding='utf-8') as f:
     config = json.load(f)
 
 def app(name):
-    return config['app'][name]
+    app = {
+        "name":"SAViD",
+        "fullname":"Sistema de Análisis y Verificación de Dietas",
+        "version":"1.0"
+    }
+    return app[name]
 
 def database(name):
-    return config['database'][name]
+    return db.db()[name]
 
 def outlook(name):
     return config['outlook'][name]
